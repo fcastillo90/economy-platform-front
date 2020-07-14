305388027a41dcd421101afb105d0bd39c5e3c6b
Francisco Castillo <fcastillog@ripley.com>
2020-07-13 23:35:12 -0400

    FEATURE - create color palette config

---
+++ src/utils/paletteConfig.js
@@ -0,0 +1,32 @@
+export default {
+  lightTheme: {
+    palette: {
+      type: 'light',
+      primary: {
+        light: '#a9d7d2',
+        main: '#2a9d8f',
+        dark: '#154e47',
+        contrastText: '#FFFFFF',
+      },
+      secondary: {
+        main: '#e76f51',
+        contrastText: '#FFFFFF',
+      },
+    },
+  },
+  darkTheme: {
+    palette: {
+      type: 'dark',
+      primary: {
+        light: '#a8b5ba',
+        main: '#264653',
+        dark: '#162a31',
+        contrastText: '#FFFFFF',
+      },
+      secondary: {
+        main: '#e76f51',
+        contrastText: '#FFFFFF',
+      },
+    },
+  },
+}
